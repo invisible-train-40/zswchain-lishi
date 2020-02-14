a6193f045c5a4a522fbebebbb173436f6b9bcd1f
Matthieu Vachon <matt@dfuse.io>
2020-02-14 16:21:25 -0500

    Fixed shared_public_key variant which must use the `to_string()` method instead of the string operator

--- libraries/chain/authority.cpp
+++ libraries/chain/authority.cpp
@@ -2,6 +2,6 @@
 
 namespace fc {
    void to_variant(const eosio::chain::shared_public_key& var, fc::variant& vo) {
-      vo = (std::string)var;
+      vo = var.to_string();
    }
 } // namespace fc
